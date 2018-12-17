ed2fa0a3480f7f85c02b1ba17af1e3fb5b658a11
tiana-horn <tiana.horn@gmail.com>
2018-12-17 15:36:59 -0500

    Co-authored-by Sean Houde <seanhoude@gmail.com> Co-authored-by TaylorHomes <taylor.homes01@gmail.com> Co-authored-by Steve Marks <marks.steven@gmail.com> Co-authored-by Tiana Horn <tiana.horn@gmail.com>

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
+class User(AbstractUser):
+    users_followed = models.ManyToManyField(to='User', through='Follow', through_fields=('following_user', 'followed_user'), related_name="followers")
+
+class Post(models.Model):
+    user = models.ForeignKey(to="User", on_delete=models.CASCADE, null=True)
+    text = models.CharField(max_length=280)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Follow(models.Model):
+    following_user = models.ForeignKey(to="User", on_delete=models.CASCADE, related_name="follows_from")
+    followed_user = models.ForeignKey(to="User", on_delete=models.CASCADE, related_name="follows_to")
+
+class Response(models.Model):
+    user = models.ForeignKey(to="User", on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE, null=True, related_name="post_response")
+    text = models.CharField(max_length=280)
+    created_at = models.DateTimeField(auto_now_add=True)
+
