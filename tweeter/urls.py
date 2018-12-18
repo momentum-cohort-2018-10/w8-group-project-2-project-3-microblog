a35a37488b478b983b8824be8133bb5c94179d5b
TaylorHomes <taylor.homes01@gmail.com>
2018-12-18 12:41:05 -0500

    Create registration / login functionality

--- tweeter/urls.py
+++ tweeter/urls.py
@@ -13,9 +13,48 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from core import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
+    path(
+        'register/',
+        TemplateView.as_view(template_name='register.html'),
+        name='register'),
+    path(
+        'accounts/password/reset/',
+        PasswordResetView.as_view(
+            template_name='registration/password_reset_form.html'),
+        name="password_reset"),
+    path(
+        'accounts/password/reset/done/',
+        PasswordResetDoneView.as_view(
+            template_name='registration/password_reset_done.html'),
+        name="password_reset_done"),
+    path(
+        'accounts/password/reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html'),
+        name="password_reset_confirm"),
+    path(
+        'accounts/password/done/',
+        PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'),
+        name="password_reset_complete"),
+    path(
+        'about/',
+        TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    path('accounts/', include('registration.backends.simple.urls')),
 ]
